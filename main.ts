a5ff4016cd673d814f8a9bbd1f9b7a9a80a4b565
franek12345678 <109067977+franek12345678@users.noreply.github.com>
2022-07-13 20:03:40 +0200

    Początkowe pliki dla projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "DICE") {
+        basic.showNumber(value)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    DICE = randint(1, 6)
+    basic.showNumber(DICE)
+    radio.sendValue("DICE", DICE)
+})
+let DICE = 0
+radio.setGroup(255)
+radio.setFrequencyBand(83)
+radio.setTransmitPower(7)
